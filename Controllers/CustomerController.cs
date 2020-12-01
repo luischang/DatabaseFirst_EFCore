0903197387c613097bc15951675053f41ad1e530
Luis Chang <luis.chang@outlook.com>
2020-11-30 22:42:39 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NETCOREM3_DatabaseFirst.DataAccess;
+using NETCOREM3_DatabaseFirst.Models;
+
+namespace NETCOREM3_DatabaseFirst.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Listado()
+        {
+            ViewBag.ListadoCliente = await DACustomer.ListadoAsync();
+            return PartialView();
+        }
+
+        public IActionResult Grabar(int idCliente,
+                string nombres, string apellidos, string pais, string ciudad, string telefono)
+        {
+            Customer customer = new Customer();
+            customer.FirstName = nombres;
+            customer.LastName = apellidos;
+            customer.City = ciudad;
+            customer.Country = pais;
+            customer.Phone = telefono;
+
+            bool exito = true;
+            if (idCliente == -1)
+                exito = DACustomer.Insertar(customer);
+            else
+            {//Es una actualización
+                customer.Id = idCliente;
+                exito = DACustomer.Actualizar(customer);
+            }
+
+            return Json(exito);
+        }
+    }
+}
